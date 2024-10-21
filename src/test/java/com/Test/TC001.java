0e632a271cf69b161f165fbb68d0e12cf3f51b30
ShachiRatnaparkhi <shachifeb20@gmail.com>
2024-10-21 10:22:25 +0530

    Added project Group3

---
+++ src/test/java/com/Test/TC001.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+public class TC001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hi, this is Group no. 3 ");
+		
+	}
+
+}
